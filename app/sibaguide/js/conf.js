a403b1e9c7195c2290bba1c64463c9e9cb446cbf
Mauricio Muriel <mauricio.muriel@calitek.net>
2019-06-17 10:13:25 -0500

    Ajustando el código de máquina a COL100, usuario base

--- app/sibaguide/js/conf.js
+++ app/sibaguide/js/conf.js
@@ -1,6 +1,6 @@
 var sgConf = {
 
-	id : 'MEX57',
+	id : 'COL100',
 	scroll: {
 		defaultMsg : 'Usted está viendo el canal SIBAGUIDE, aquí encontrerá toda la información sobre toda la programación emitida por nuestro sistema de cable',
 		speed: 5,//Letras por segundo	
